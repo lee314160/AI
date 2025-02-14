6fa5564230d31dd0a56538c92e6d4aaae4937494
DESKTOP-1JKGPOK\lee <1193317449@qq.com>
2025-02-14 15:41:07 +0800

    新增外盘商品期货

--- AI/sina_futures.py
+++ AI/sina_futures.py
@@ -7,6 +7,7 @@ urls = [
     "https://rewards.bing.com/",
     "https://finance.sina.com.cn/",
     "https://finance.sina.com.cn/futuremarket/",
+    "https://finance.sina.com.cn/money/future/hf.html",
     "https://finance.sina.com.cn/futures/quotes/I0.shtml",
     "https://finance.sina.com.cn/futures/quotes/JM0.shtml",
     "https://finance.sina.com.cn/futures/quotes/Y0.shtml",
